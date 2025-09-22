b023cfa838cc3c1bbf32e084f062573c25d8da58
nikhil-kumar2712 <nk8201059@gmail.com>
2025-09-22 17:48:59 +0530

    CSS updates

---
+++ frontend/src/pages/HomeAfterSignIn.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import SignUp from "../components/signUp";
+import SignIn from "../components/signIn";
+import Slides from "../components/slides";
+import Services from "../components/services";
+
+function Home() {
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  return (
+    <div>
+    
+      <Header setShowSignUp={setShowSignUp} setShowSignIn={setShowSignIn} />
+      {/* Sign Up Modal */}
+      {showSignUp && <SignUp onClose={() => setShowSignUp(false)} />}
+      {/* Sign In Modal */}
+      {showSignIn && <SignIn onClose={() => setShowSignIn(false)} />}
+
+      {/* Hero Slides */}
+      <Slides />
+
+      {/* Services Section */}
+      <Services />
+
+      {/* Quote */}
+      <p style={{ textAlign: "center", paddingTop: "3%" , paddingLeft: "100px", paddingRight: "100px", paddingBottom: "3rem", fontSize: "1.5rem" }}>
+        <i>
+          "Ready to embark on your next adventure? Escape to breathtaking destinations with unbeatable deals from <strong>Vtravellers</strong>. From sun-soaked beaches to cultural treasures, we've got your dream getaway covered. Book now and unlock a world of unforgettable experiences!"
+        </i>
+      </p>
+
+      {/* Book Now Button */}
+      <div className="button">
+        <a href="/booking" style={{ textDecoration: "none" , whiteSpace: "nowrap" }}>BOOK NOW</a>
+      </div>
+
+      {/* Footer */}
+      <Footer />
+
+    </div>
+  );
+}
+
+export default Home;
